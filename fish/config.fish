ffb6f889cb3643460d2757280255b98b51f175ce
huajie06 <huajie06@gmail.com>
2026-06-28 23:27:11 -0400

    fish shell rm welcome msg

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 set fish_greeting
 
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Inherit PATH from bash login shell
+    bash -l -c 'echo "$PATH"' 2>/dev/null | head -1 | read -l bash_path
+    if test -n "$bash_path"
+        set -gx PATH $bash_path
+    end
 end
