95363ae2bdc8b73a6313fed1299bef068c323f54
huajie06 <huajie06@gmail.com>
2026-07-04 21:41:44 -0400

    display-line-numbers-width as 3

--- emacs/lisp/init-core.el
+++ emacs/lisp/init-core.el
@@ -53,7 +53,9 @@
   (global-eldoc-mode -1)
   (electric-pair-mode 1)
   (electric-indent-mode 1)
-  (setq display-line-numbers-type 'relative)
+  (setq display-line-numbers-type 'relative
+        display-line-numbers-width 3
+        display-line-numbers-grow-only t)
   (global-display-line-numbers-mode t)
   (global-auto-revert-mode 1)
 
